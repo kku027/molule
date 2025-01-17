3ed6ae9ccfeea2da3a949fa31c5ea20031e87c25
Кострубов Константин <kostrybov@mail.ru>
2025-01-17 23:24:39 +0300

    123

---
+++ module_3_hard.py
@@ -0,0 +1,16 @@
+def calculate_structure_sum(*args):
+    count = 0
+    for arg in args:
+        if isinstance(arg, list) or isinstance(arg, tuple) or isinstance(arg, set):
+            count += calculate_structure_sum(*arg)
+        elif isinstance(arg, dict):
+            count += calculate_structure_sum(*arg.items())
+        elif isinstance(arg, str):
+            count += len(arg)
+        elif isinstance(arg, int):
+            count += arg
+    return count
+
+
+lst = [[1, 2, 3], {'a': 4, 'b': 5}, (6, {'cube': 7, 'drum': 8}), "Hello", ((), [{(2, 'Urban', ('Urban2', 35))}])]
+print(calculate_structure_sum(lst))
